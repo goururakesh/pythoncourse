c4afb0f4e6e8f82ba1be19306902057047f695bd
Rakesh <goururakesh18@gmail.com>
2025-03-12 14:51:48 +0530

    hello

---
+++ ifelifelseexample.py
@@ -0,0 +1,12 @@
+weather = "snowy"
+time_of_day = "night"
+if weather == "sunny":
+    print("you can play with your car toy")
+elif weather == "rainy":
+    print("you can play with your boat toy")
+elif weather == "snowy" and time_of_day == "night":
+    print("you and play with your teddy bear toy ")
+else:
+    print("you can play with your snowman toy")
+
+print("stay warm and have a great time!!!")
